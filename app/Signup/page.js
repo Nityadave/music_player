583e15ca8d41260f3ed3c9d47e9effb7d63b917e
Nitya Dave <nityadave07@gmail.com>
2023-08-04 16:32:00 +0530

    changes

--- app/Signup/page.js
+++ app/Signup/page.js
@@ -1,19 +1,90 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
+import eye_open from '../../public/eye_open.png';
+import eye_close from '../../public/eye_close.png';
+import Image from 'next/image';
+
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordInputType, setPasswordInputType] = useState('password');
+
+  const handleSubmit = async (e) => {
+    // submit form
+    // const query = `select * from login where email = ${email} and password =  ${password}`
+    // const result = executeQuery({query});
+    // if(result){
+    //     alert("Work done");
+    // }
+  };
+
+  const isButtonDisabled =
+    !email ||
+    !password ||
+    !confirmPassword ||
+    password !== confirmPassword ||
+    !/\S+@\S+\.\S+/.test(email);
+
+  const togglePasswordVisibility = () => {
+    setPasswordInputType(passwordInputType === 'password' ? 'text' : 'password');
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.background}></div>
-      <div className={styles.signupCard}>
-        <h1 className={styles.headline}>Sign Up</h1>
-        <form>
-          <input className={styles.datainput} type="text" placeholder="Email" />
-          <input className={styles.datainput} type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <button className={styles.button}type="submit">Login</button>
+      <div className={styles.loginCard}>
+        <h1 className={styles.headline}>Signup</h1>
+        {/* {error && <p>{error}</p>} */}
+        <form onSubmit={handleSubmit}>
+          <input
+            className={styles.datainput}
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.datainput}
+              type={passwordInputType}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={styles.toggleButton}
+            >
+              <Image
+                src={
+                  passwordInputType === 'password'
+                    ? eye_close
+                    : eye_open
+                }
+                height={30}
+                width={29}
+                alt="toggle"
+              />
+            </button>
+          </div>
+          <input
+            className={styles.datainput}
+            type={passwordInputType}
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button
+            className={styles.button}
+            type="submit"
+            disabled={isButtonDisabled}
+          >
+            Signup
+          </button>
         </form>
       </div>
     </div>
   );
-};
-
+}
